0954dcf870d567f8f17ccedcf91d84d19912c6b2
TH WU <federalkpl@gmail.com>
2016-02-29 21:08:24 +0800

    The location info of the object contained in the image could now be correctly written to the text description file.

--- app/src/main/java/wu/th/camerapreview/MainActivity.java
+++ app/src/main/java/wu/th/camerapreview/MainActivity.java
@@ -1,32 +1,50 @@
 package wu.th.camerapreview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.util.Size;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.security.Policy;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.jar.Manifest;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnTouchListener, SurfaceHolder.Callback{
     static String TAG = "Main";
+    String mCurrentPhotoPath;
 
     SurfaceView view, rect;
+    Button button01;
     private Camera mCamera;
 
     int left, top, right, bottom;
+    int pic_object_x, pic_object_y, pic_object_width, pic_object_height;
     int canvas_width, canvas_height;
 
     Paint paint;
@@ -40,6 +58,18 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
         paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(3);
+
+        button01 = (Button) findViewById(R.id.button01);
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread() {
+                    public void run() {
+                        TakePhoto_And_Generate_Info();
+                    }
+                }.start();
+            }
+        });
 
         view = (SurfaceView) findViewById(R.id.surface_view);
         view.getHolder().addCallback(this);
@@ -117,6 +147,10 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
         try {
             c = Camera.open(0); // attempt to get a Camera instance
             c.setDisplayOrientation(90);
+            Camera.Parameters p = c.getParameters();
+            p.setRotation(90);
+            p.setPictureSize(640, 480);
+            c.setParameters(p);
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
@@ -160,6 +194,11 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
 
             canvas.drawRect(left, top, right, bottom, paint);
             rect.getHolder().unlockCanvasAndPost(canvas);
+
+            pic_object_x = left * 480 / view.getWidth();
+            pic_object_y = top * 640 / view.getHeight();
+            pic_object_width = (right - left) * 480 / view.getWidth();
+            pic_object_height = (bottom - top) * 640 / view.getHeight();
         }
         else if (event.getAction() == MotionEvent.ACTION_MOVE){
             right = (int) event.getX();
@@ -172,5 +211,115 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
             rect.getHolder().unlockCanvasAndPost(canvas);
         }
         return true;
+    }
+
+    /*private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStorageDirectory(), "Positive Samples");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    } */
+
+    public void TakePhoto_And_Generate_Info(){
+        Camera.PictureCallback pictureCB = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera cam) {
+                File picFile = Get_Output_Directory();
+                if (picFile == null) {
+                    Log.e(TAG, "Couldn't create media file; check storage permissions?");
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(picFile);
+                    fos.write(data);
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    Log.e(TAG, "File not found: " + e.getMessage());
+                    e.getStackTrace();
+                } catch (IOException e) {
+                    Log.e(TAG, "I/O error writing file: " + e.getMessage());
+                    e.getStackTrace();
+                }
+
+                //Avoid freezing
+                mCamera.startPreview();
+            }
+        };
+        mCamera.takePicture(null, null, pictureCB);
+    }
+
+    public File Get_Output_Directory(){
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStorageDirectory(), "Positive Samples/pos_img");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+
+        File image = null;
+
+        try {
+            image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Write to the data file
+        generateInfoOnSD("pos_img.txt ", "img\\" + imageFileName + " 1 " + String.valueOf(pic_object_x) + " " + String.valueOf(pic_object_y) + " " + String.valueOf(pic_object_width) + " " + String.valueOf(pic_object_height) + "\n");
+
+        return image;
+    }
+
+    public void generateInfoOnSD(String sFileName, String content){
+        try
+        {
+            File root = new File(Environment.getExternalStorageDirectory(), "Positive Samples");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile, true);
+            writer.append(content);
+            writer.flush();
+            writer.close();
+        } catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
