be2a00ab81a8d34880238c8027382b5516b45bd1
TH WU <federalkpl@gmail.com>
2016-03-13 00:26:25 +0800

    Now the project has been modified to send video frames to Java PC server.

--- app/src/main/java/wu/th/camerapreview/MainActivity.java
+++ app/src/main/java/wu/th/camerapreview/MainActivity.java
@@ -2,11 +2,13 @@ package wu.th.camerapreview;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,11 +22,17 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 import java.security.Policy;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -32,17 +40,22 @@ import java.util.List;
 import java.util.jar.Manifest;
 
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnTouchListener, SurfaceHolder.Callback{
+public class MainActivity extends Activity implements View.OnTouchListener, TextureView.SurfaceTextureListener{
     static String TAG = "Main";
     String mCurrentPhotoPath;
+    String eduroam_pc_ip = "10.89.131.94";
+    String myRouter_pc_ip = "192.168.1.101";
 
-    SurfaceView view, rect;
+    SurfaceView rect;
+    TextureView view;
     Button button01;
     private Camera mCamera;
 
+    int count = 0;
     int left, top, right, bottom;
     int pic_object_x, pic_object_y, pic_object_width, pic_object_height;
     int canvas_width, canvas_height;
@@ -59,7 +72,8 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(3);
 
-        button01 = (Button) findViewById(R.id.button01);
+        //This part is for the button implementaion
+        /*button01 = (Button) findViewById(R.id.button01);
         button01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,12 +83,16 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
                     }
                 }.start();
             }
-        });
+        });  */
 
-        view = (SurfaceView) findViewById(R.id.surface_view);
-        view.getHolder().addCallback(this);
+        view = (TextureView) findViewById(R.id.surface_view);
+        view.setSurfaceTextureListener(this);
 
-        rect = (SurfaceView) findViewById(R.id.rectangle);
+        //view.getHolder().addCallback(this);
+
+
+        //This part is for drawing a rectangle on the second surface for generating positive cascade training samples purpose
+        /*rect = (SurfaceView) findViewById(R.id.rectangle);
         rect.setOnTouchListener(this);
         rect.getHolder().setFormat(PixelFormat.TRANSPARENT);
         rect.getHolder().addCallback(new SurfaceHolder.Callback() {
@@ -84,8 +102,9 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
 
                 canvas_width = canvas.getWidth();
                 canvas_height = canvas.getHeight();
+                Log.i(TAG,"Width & height: " + String.valueOf(canvas_width) + " " + String.valueOf(canvas_height));
 
-                canvas.drawRect(canvas.getWidth()/4, canvas.getHeight()/4, canvas.getWidth()*3/4, canvas.getHeight()*3/4, paint);
+                canvas.drawRect(canvas.getWidth() / 4, canvas.getHeight() / 4, canvas.getWidth() * 3 / 4, canvas.getHeight() * 3 / 4, paint);
                 rect.getHolder().unlockCanvasAndPost(canvas);
             }
 
@@ -98,10 +117,11 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
             public void surfaceDestroyed(SurfaceHolder holder) {
 
             }
-        });
+        }); */
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
+    //This part is for original SurfaceView (variable = view) set up
+    /*public void surfaceCreated(SurfaceHolder holder) {
         mCamera = getCameraInstance();
         try {
             mCamera.setPreviewDisplay(holder);
@@ -140,6 +160,102 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
         } catch (Exception e){
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
+    }  */
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height){
+        mCamera = getCameraInstance();
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height){
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface){
+        if(mCamera != null)
+            mCamera.release();
+        return false;
+    }
+
+    public void onSurfaceTextureUpdated(SurfaceTexture surface){
+
+        if (count < 5){
+            count++;
+            return;
+        }
+        else{
+            count = 0;
+            new Thread(){
+                public void run(){
+                    int i = 0;
+                    DataOutputStream dos = null;
+
+                    //Get bitmap
+                    Bitmap frame_bmp = view.getBitmap();
+
+                    //Send the bitmap to PC
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    frame_bmp.compress(Bitmap.CompressFormat.JPEG, 50,stream);
+                    byte[] byteArray = stream.toByteArray();
+                    InputStream is = new ByteArrayInputStream(byteArray);
+                    BufferedInputStream bis = new BufferedInputStream(is);
+
+                    Socket socket = null;
+                    try {
+                        socket = new Socket(myRouter_pc_ip, 8080);
+                        dos = new DataOutputStream(socket.getOutputStream());
+                        while ((i = bis.read()) > -1)
+                            dos.write(i);
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    finally{
+                        try{
+                            bis.close();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        if (dos != null){
+                            try{
+                                dos.flush();
+                                dos.close();
+                            }
+                            catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                        if (socket != null){
+                            try{
+                                socket.close();
+                            }
+                            catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }.start();
+        }
+
     }
 
     public static Camera getCameraInstance(){
@@ -149,6 +265,9 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
             c.setDisplayOrientation(90);
             Camera.Parameters p = c.getParameters();
             p.setRotation(90);
+
+            List<Camera.Size> t = p.getSupportedPictureSizes();
+
             p.setPictureSize(640, 480);
             c.setParameters(p);
         }
@@ -195,10 +314,11 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
             canvas.drawRect(left, top, right, bottom, paint);
             rect.getHolder().unlockCanvasAndPost(canvas);
 
-            pic_object_x = left * 480 / view.getWidth();
-            pic_object_y = top * 640 / view.getHeight();
-            pic_object_width = (right - left) * 480 / view.getWidth();
-            pic_object_height = (bottom - top) * 640 / view.getHeight();
+            //Normalize the coordinate info
+            pic_object_x = left * 480 / rect.getWidth();
+            pic_object_y = top * 640 / rect.getHeight();
+            pic_object_width = (right - left) * 480 / rect.getWidth();
+            pic_object_height = (bottom - top) * 640 / rect.getHeight();
         }
         else if (event.getAction() == MotionEvent.ACTION_MOVE){
             right = (int) event.getX();
@@ -280,7 +400,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
 
     public File Get_Output_Directory(){
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "Positive_Sample_" + timeStamp;
         File storageDir = new File(Environment.getExternalStorageDirectory(), "Positive Samples/pos_img");
         if (!storageDir.exists()) {
             storageDir.mkdirs();
@@ -300,7 +420,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, Surf
         }
 
         //Write to the data file
-        generateInfoOnSD("pos_img.txt ", "img\\" + imageFileName + " 1 " + String.valueOf(pic_object_x) + " " + String.valueOf(pic_object_y) + " " + String.valueOf(pic_object_width) + " " + String.valueOf(pic_object_height) + "\n");
+        generateInfoOnSD("pos_img.txt ", "pos_img\\" + image.getName() + " 1 " + String.valueOf(pic_object_x) + " " + String.valueOf(pic_object_y) + " " + String.valueOf(pic_object_width) + " " + String.valueOf(pic_object_height) + "\n");
 
         return image;
     }
